d788202952c68f8b34c62b558357dc46b7190d9b
Shriyash Jagtap <jagtapshriyash4@gmail.com>
2024-10-31 21:22:19 +0530

    added matadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,11 @@
 // app/layout.tsx
-"use client";
+
 import './globals.css'
 import { ReactNode } from 'react'
 import { AuthProvider } from '@/context/AuthContext';
-
+export const metadata = {
+  title: 'Quizify',
+  description: 'Quiz Practice Platform for IITM BS Students',}
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
